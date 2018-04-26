8d3aaaa4d3f3568248763838402cfee0b39197f0
Lars Erling Quist Lynnerup <lars9865@edu.campusvejle.dk>
2018-04-26 15:08:15 +0200

    v0.2

---
+++ DbAccess/CommonDB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DbAccess
+{
+    public abstract class CommonDB
+    {
+        //Fields
+        private readonly string connectionString;
+
+        public CommonDB(string conString)
+        {
+            connectionString = conString;
+        }
+
+        public DataSet ExecuteQuery(string query, CommandType cType = CommandType.Text) //Så CommandType er som standard = Text, og jeg behøver ikke at give det med som parameter. MEN hvis jeg bruger en StoredProcedure, så skal jeg give CommandType.StoredProcedure med på en måde. Og det kommer jeg til at skulle fordi at dataset skal nok hente alle tabeller ud.
+        {
+            //Bliver brugt til at eksekvere inserts på databasen, den modtager SQL-strengen som skal insertes
+        }
+
+        public bool ExecuteNonQuery(string query)
+        {
+            
+        }
+    }
+}
---
+++ DbAccess/DBHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess
+{
+    public class DBHandler : CommonDB
+    {
+        public DBHandler(string conString) : base (conString) //Smider den det automatisk videre til base class (altså CommonDB), eller skal jeg også have et connectionString field i denne klasse?
+        {
+            
+        }
+
+
+    }
+}
